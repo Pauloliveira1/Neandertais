88b929d3b02481c88be189bdbf59484ca95ba189
Pauloliveira1 <neoxpla54666@gmail.com>
2025-06-17 09:16:25 -0300

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyBtL4JTvftbw2aHwYd-zRnfhsxCOrx_6cI",
+  authDomain: "plux-998a6.firebaseapp.com",
+  databaseURL: "https://plux-998a6-default-rtdb.firebaseio.com",
+  projectId: "plux-998a6",
+  storageBucket: "plux-998a6.firebasestorage.app",
+  messagingSenderId: "639722927532",
+  appId: "1:639722927532:web:a15a1a3355faf15ad33609",
+  measurementId: "G-W0Y5D7SVF5"
+};
+
+firebase.initializeApp(firebaseConfig);
